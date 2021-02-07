21e2c931b70da8ac0f57b3391f384cb1c6d4d68f
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-02-07 20:01:17 +0200

    codestyle bugs fixed

--- src/scripts/components/renderCalendar/calendarBody/teamBodyCell.js
+++ src/scripts/components/renderCalendar/calendarBody/teamBodyCell.js
@@ -1,10 +1,10 @@
-class TeamBodyCell{
+class TeamBodyCell {
   constructor(dayString){
     this.dayString = dayString;
   }
   render() {
-    return `<td class=" team-body__cell ${this.dayString === 'Sa' || this.dayString === 'Su' ? 'cell-gray' : ''} "></td>`
+    return `<td class=" team-body__cell ${this.dayString === 'Sa' || this.dayString === 'Su' ? 'cell-gray' : ''} "></td>`;
   };
-};
+}
 
 export default TeamBodyCell;
